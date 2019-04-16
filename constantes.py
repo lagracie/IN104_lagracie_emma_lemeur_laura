dd960bdcb49e8f17c049dae2b2984dfd9530b946
lagracie <48725852+lagracie@users.noreply.github.com>
2019-04-16 15:47:14 +0200

    Add files via upload

---
+++ constantes.py
@@ -0,0 +1,23 @@
+"""Constantes du jeu pacman"""
+
+#Paramètres de la fenêtre
+nombre_sprite_cote = 17
+taille_sprite = 30
+cote_fenetre = nombre_sprite_cote * taille_sprite
+
+
+#Personnalisation de la fenêtre
+titre_fenetre = "Pacman Game"
+image_icone = "pacman_droite.jpg"
+
+#Listes des images du jeu
+image_accueil = "background.jpg"
+image_fond = "background.jpg"
+image_mur = "mur.png"
+image_depart = "depart.png"
+droite="pacman_droite.jpg"
+gauche="pacman_gauche.jpg"
+haut="pacman_haut.jpg"
+bas="pacman_bas.jpg"
+rond="rond.jpg"
+
